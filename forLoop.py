e2cfca3c03eb29256611dd8ee62e4a21a9b9a868
albakir13 <albakir13@MacBook-Pro.local>
2021-03-25 20:27:16 +0300

    forloop the second file

---
+++ forLoop.py
@@ -0,0 +1,8 @@
+frinds = [ 'Abdulaziz', 'Abdullah', 'Shehab' ]
+
+for frinds in frinds:
+    print('Happy birth day: ', frinds)
+
+print('wish you all the best in the future')
+print('and always take care of your self')
+# Math-test
